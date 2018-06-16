2ddb126c778cac5e39816c9b9504207485605ca1
thegodfather <AliFattahiproject@gmail.com>
2018-06-17 01:25:41 +0430

    Init commit

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ var TAG = '-------------server.js ';
 const express = require('express');
 const path = require('path');
 const publicPath = path.join(__dirname,'../public');
-const port = process.env.PORT || 3000;
+
  var app = express();
+const port = process.env.PORT || 3000;
  app.use(express.static(publicPath));
 
  app.listen(port,()=>{
